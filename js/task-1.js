76836590ab30c41816bbbe8df2755c216176817f
maksymPr424 <m.prykhodko424@gmail.com>
2024-04-13 19:50:19 +0200

    task-3

--- js/task-1.js
+++ js/task-1.js
@@ -41,3 +41,23 @@
 
 // console.log(foo([5, 5, 5]));
 // console.log(foo([5, 5, 5]));
+
+// TODO  ======= 3 =======
+
+// Create a function with two arguments that will return an array of the first n multiples of x.
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+// Return the results as an array or list ( depending on language ).
+// ExamplescountBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+// const ExamplescountBy = (a, b) => {
+//     const result = []
+//     for (let i = 1; i <= b; i++) {
+//         result.push(a * i)
+//     }
+//     return result
+// }
+
+// console.log(ExamplescountBy(2,5));
+
+// TODO  ======= 4 =======
